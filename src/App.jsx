bbf299d4dd6b3b51c63f6ba88a98a50d7a4c31aa
AvnSuyash <ng20654@gmail.com>
2025-06-02 17:43:59 +0530

    Some components of crypto tracker

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Banner from "./Components/Banner/Banner";
+import CoinTable from "./Components/CoinTable/CoinTable";
+import Navbar from "./Components/NavBar/Navbar";
+
+
+function App(){
+  return(
+    <div>
+      <Navbar/>
+      <Banner/>
+      <CoinTable/>
+    </div>
+  );
+}
+export default App;
---
+++ src/helpers/axiosInstance.js
@@ -0,0 +1,5 @@
+import axios from "axios"
+const axiosInstance=axios.create({
+    baseURL:'https://docs.coingecko.com/v3.0.1',
+    
+})
