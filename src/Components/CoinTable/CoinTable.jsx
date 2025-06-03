ee8d49f68af513541fa65bbdd5ec2d19631dccd1
AvnSuyash <ng20654@gmail.com>
2025-06-03 12:11:30 +0530

    Added coin table ui with cache

--- src/Components/CoinTable/CoinTable.jsx
+++ src/Components/CoinTable/CoinTable.jsx
@@ -1,8 +1,79 @@
-function CoinTable(){
-    return(
-        <>
-        Coin Table
-        </>
+import { useEffect, useState } from "react";
+import { fetchCoinData } from "../../services/fetchCoinData";
+import { useQuery } from "@tanstack/react-query";
+
+function CoinTable() {
+    const [page, setPage] = useState(1);
+
+    const {
+        data,
+        isLoading,
+        isError,
+        error,
+        isFetching,
+    } = useQuery({
+        queryKey: ['coins', page],
+        queryFn: () => fetchCoinData(page, 'usd'),
+        cacheTime: 1000 * 60 * 20,
+        staleTime:1000 *60 *20,
+    });
+
+
+    if (isError) {
+        return <div>Error: {error.message}</div>;
+    }
+
+    return (
+        <div className="flex flex-col items-center justify-center mx-auto my-5 gap-4 w-[80vw]">
+            <div className="flex items-center justify-center w-full px-3 py-2 font-semibold text-black bg-yellow-300">
+                <div className="w-[35%]">Coin</div>
+                <div className="w-[25%]">Price</div>
+                <div className="w-[20%]">24h Change</div>
+                <div className="w-[20%]">Market Cap</div>
+            </div>
+
+            <div className="flex flex-col w-full mx-auto">
+                {isLoading && <div>Loading...</div>}
+                {
+                    
+                    data && data.map((coin) => {
+                        return (
+                            <div key={coin.id} className="flex items-center justify-between px-2 py-4 font-semibold text-white bg-transparent">
+                                <div className="flex items-center gap-3 w-[35%]">
+                                    <div className="w-[5rem] h-[5rem]">
+                                        <img src={coin.image} className="w-full h-full" />
+                                    </div>
+                                    <div className="flex flex-col">
+                                        <div className="text-3xl">{coin.name}</div>
+                                        <div className="text-xl">{coin.symbol}</div>
+                                    </div>
+                                </div>
+                                <div className="w-[25%]">{coin.high_24h}</div>
+                                <div className="w-[20%]">{coin.price_change_24h}</div>
+                                <div className="w-[20%]">{coin.market_cap}</div>
+                            </div>
+                        );
+                    })
+                }
+            </div>
+
+            <div className="flex items-center justify-center gap-4">
+                <button
+                    className="text-2xl text-white btn btn-primary btn-wide"
+                    onClick={() => setPage(page - 1)}
+                    disabled={page === 1}
+                >
+                    Prev
+                </button>
+                <button
+                    className="text-2xl text-white btn btn-secondary btn-wide"
+                    onClick={() => setPage(page + 1)}
+                >
+                    Next
+                </button>
+            </div>
+        </div>
     );
 }
+
 export default CoinTable;
--- src/helpers/axiosInstance.js
+++ src/helpers/axiosInstance.js
@@ -1,5 +1,7 @@
-import axios from "axios"
-const axiosInstance=axios.create({
-    baseURL:'https://docs.coingecko.com/v3.0.1',
-    
-})
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: 'https://api.coingecko.com/api/v3',
+});
+
+export default axiosInstance;
